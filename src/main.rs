8d04a2c744d92964e8fbd0c7c17c72df1212a8e9
APCode <josealpama98@hotmail.com>
2024-08-07 13:57:10 -0500

    feat: Add routes for info, palabras par, and service the assets

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod words;
+mod pairs;
+
+use actix_files;
 use actix_web::{
     get, post, delete, put, patch, web,
     App, HttpResponse, HttpServer, Responder
@@ -7,6 +11,7 @@ use lazy_static::lazy_static;
 use tera::Tera;
 
 const SOURCE: &str = "templates/**/*";
+
 // * Initialize the Tera template engine
 lazy_static! {
     // * Create a static reference to the Tera instance
@@ -20,7 +25,7 @@ lazy_static! {
 async fn index() -> impl Responder {
     // * Create a context to pass to the template
     let mut context = tera::Context::new();
-    // * Add a key-value pair to the context
+    // * Send a message to the template
     context.insert("rustacean_message", "Hello from Rust back!!");
     // * Render the template with the context
     let page_content = TEMPLATES.render("index.html", &context).unwrap();
@@ -28,13 +33,51 @@ async fn index() -> impl Responder {
     HttpResponse::Ok().body(page_content)
 }
 
+#[get("/info")]
+async fn info() -> impl Responder {
+    // * Create a context to pass to the template
+    let context = tera::Context::new();
+    // * Render the template with the context
+    let page_content = TEMPLATES.render("info.html", &context).unwrap();
+    // * Return the rendered template as the response
+    HttpResponse::Ok().body(page_content)
+}
+
+#[get("/palabras")]
+async fn get_words() -> impl Responder {
+    // * Get all the words
+    let word_list = words::Word::get_words().get_random_words();
+    let mut context = tera::Context::new();
+    context.insert("word1", &word_list.spanish);
+    context.insert("word2", &word_list.english);
+    let page_content = TEMPLATES.render("words.html", &context).unwrap();
+    HttpResponse::Ok().body(page_content)
+}
+
+#[get("/par")]
+async fn get_pair() -> impl Responder {
+    // * Get all the words
+    let word_list = words::Word::get_words().get_random_words();
+    let mut context = tera::Context::new();
+    context.insert("word1", &word_list.spanish);
+    context.insert("word2", &word_list.english);
+    let page_content = TEMPLATES.render("pair.html", &context).unwrap();
+    HttpResponse::Ok().body(page_content)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
 
-    // * Start the server with the following routes/services
+    // * Start the server with the following routes/services on port 9090
     HttpServer::new(|| {
             App::new()
                 .service(index)
+                .service(info)
+                .service(get_words)
+                .service(get_pair)
+                .service(actix_files::Files::new("/assets", "./assets")
+                    .show_files_listing()
+                )
         })
         .bind(("127.0.0.1", 9090))?
         .run()
